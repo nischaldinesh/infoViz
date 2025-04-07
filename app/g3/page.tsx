d6cc1979c1d64b74b2989084cf6af52e5614104d
Nischal Dinesh <nischaldgowda77@gmail.com>
2025-04-07 16:54:37 -0500

    created 4 pages for the each of them to work on the graphs

---
+++ app/g3/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Graph 3</div>;
+};
+
+export default page;
---
+++ app/g4/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Graph 4</div>;
+};
+
+export default page;
